64aefd14da5bcf932e5b96a9e8242d5db93fd1b3
hp04301986 <hp393747035@126.com>
2018-01-18 10:46:12 +0800

    update add two numbers in linked list.

--- src/com/hpe/hp/AddTwoNums.java
+++ src/com/hpe/hp/AddTwoNums.java
@@ -15,40 +15,30 @@ Explanation: 342 + 465 = 807.
 public class AddTwoNums {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-	    int carry = 0;
-	    ListNode p, dummy = new ListNode(0);
-	    p = dummy;
-	    while (l1 != null || l2 != null || carry != 0) {
-	        if (l1 != null) {
-	            carry += l1.val;
-	            l1 = l1.next;
-	        }
-	        if (l2 != null) {
-	            carry += l2.val;
-	            l2 = l2.next;
-	        }
-	        p.next = new ListNode(carry++);
-	        carry /= 10;
-	        p = p.next;
-	    }
-	    return dummy.next;
-	}
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+        	ListNode cur = l1 !=null ? l1 : (l2 != null ? l2 : (new ListNode(0)));
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+            
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
+    }
 	
 	public static void main(String[] args) {
-		ListNode l1 = new ListNode(0);
-		l1.val = 3;
-		l1.next = new ListNode(0);
-		l1.val = 4;
-		l1.next = new ListNode(0);
-		l1.val = 2;
-		ListNode l2 = new ListNode(0);
-		l2.val = 4;
-		l2.next = new ListNode(0);
-		l2.val = 6;
-		l2.next = new ListNode(0);
-		l2.val = 5;
-		
-		System.out.println(new AddTwoNums().addTwoNumbers(l1, l2).next.val);
+		ListNode l1 = new ListNode(3);
+		l1.val = 342;
+		ListNode l2 = new ListNode(3);
+		l2.val = 465;
+		ListNode result = new AddTwoNums().addTwoNumbers(l1, l2);
+		System.out.println(result.val+", "+ result.next.val + ", "+result.next.next.val);
 	}
 
 }
