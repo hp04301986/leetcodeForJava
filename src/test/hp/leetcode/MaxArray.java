49c5ab7dc5d8c1404f1f87314457d231f9ec2333
hp04301986 <hp393747035@126.com>
2018-04-06 18:30:31 +0800

    get max data in a window

---
+++ src/test/hp/leetcode/MaxArray.java
@@ -0,0 +1,61 @@
+package test.hp.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * 有一个整型数组arr和一个大小为w的窗口从数组的最左边滑到最右边，窗口每次向右边滑一个位置
+ * 例如：数组为[4,3,5,4,3,3,6,7]，窗口大小为3时，
+ * [4 3 5] 4 3 3 6 7 窗口中最大值为5 
+ * 4 [3 5 4] 3 3 6 7 窗口中最大值为5
+ * 4 3 [5 4 3] 3 6 7 窗口中最大值为5
+ * 4 3 5 [4 3 3] 6 7 窗口中最大值为4
+ * 4 3 5 4 [3 3 6] 7 窗口中最大值为6
+ * 4 3 5 4 3 [3 6 7] 窗口中最大值为7
+ * 如果数组长度为n，窗口大小为w，则一共产生n-w+1个窗口的最大值
+ * 请实现一个函数，
+ * 输入：整型数组arr，窗口大小为w
+ * 输出：一个长度为n-w+1的数组res，res[i]表示每种窗口状态下的最大值
+ * 以本题为例，结果应该返回{5, 5, 5, 4, 6, 7}
+ * 
+ * @author heping
+ *
+ */
+public class MaxArray {
+
+	public MaxArray() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static int[] getMaxWindow(int[] arr, int w) {
+		if(arr == null || w < 1 || arr.length < w) {
+			return null;
+		}
+		LinkedList<Integer> qmax = new LinkedList<Integer>();
+		int[] res = new int[arr.length - w + 1];
+		int index = 0;
+		for(int i = 0; i < arr.length; i ++) {
+			while(!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[i]) {
+				qmax.pollLast();
+			}
+			qmax.addLast(i);
+			//过期下标移除
+			if(qmax.peekFirst() == i - w) {
+				qmax.pollFirst();
+			}
+			if(i >= w-1) {
+				res[index++] = arr[qmax.peekFirst()];
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {4,3,5,4,3,3,6,7};
+		int w = 3;
+		int[] res = getMaxWindow(a, w);
+		for(int r : res) {
+			System.out.println(r);
+		}
+	}
+
+}
